ad69600f3f8b08f05de2fd53ca08f1dd8d7fed80
S M <malapit66@gmail.com>
2020-10-31 04:06:19 -0500

    created implicit index

--- template-middleware.js
+++ template-middleware.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
+const path   = require('path');
 const ejs    = require('ejs');
 const fs     = require('fs');
+const { pathToFileURL } = require('url');
 
 const template = fs.readFileSync('template.ejs').toString();
-
-// TODO: Implement implicit index
 
 router.use((req, res, next) => {
   if (/\.template\.html$/.test(req.url)) { res.status(404).end(); }
@@ -12,23 +12,22 @@ router.use((req, res, next) => {
 });
 
 router.use((req, res, next) => {
-  let _, name, extension;
-  try {
-    ([_, name, extension] = req.url.match(/(.*)\.(.*)$/));
-  } catch (e) {
-    return res.status(404), next();
-  }
-  
-  if (extension !== 'html') { return next(); }
-  
-  const filename = `${__dirname}/public/${name}.template.html`;
-  
-  if (!fs.existsSync(filename)) { return res.status(404), next(); }
+  const filename = path.join(__dirname, 'public', req.path, 'index.template.html');
+  console.log(filename);
 
-  const file = fs.readFileSync(filename).toString();
-  const [__, head, body] = file.match(/(?:<head>([\s\S]*)<\/head>[\s\S]*)?<body>([\s\S]*)<\/body>/);
-  
-  return res.send(ejs.render(template, { head, body }));
+  if (!fs.existsSync(filename)) {
+    return next();
+  }
+
+  const file = fs.readFileSync(filename, 'utf-8');
+
+  res.send(renderTemplate(template, file));
 });
 
 module.exports = router;
+
+function renderTemplate(template, file) {
+  const [, head, body] = file.match(/(?:<head>([\s\S]*)<\/head>[\s\S]*)?<body>([\s\S]*)<\/body>/);
+
+  return ejs.render(template, { head, body });
+}
