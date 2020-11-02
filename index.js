07eb43beef19eb414d423653a8fad40c69e5bfb1
S M <malapit66@gmail.com>
2020-11-02 00:22:32 -0600

    finalized some things

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const templateMiddleware = require('./template-middleware');
 
 app.use(templateMiddleware);
 
-app.use(express.static('public'));
-
 app.listen(PORT, (err) => {
   if (err) { throw err; }
   console.log(`App is listening on port ${PORT}`);
--- template-middleware.js
+++ template-middleware.js
@@ -1,33 +1,36 @@
-const router = require('express').Router();
-const path   = require('path');
-const ejs    = require('ejs');
-const fs     = require('fs');
+const express = require('express')
+const router  = express.Router();
+const path    = require('path');
+const ejs     = require('ejs');
+const fs      = require('fs');
 const { pathToFileURL } = require('url');
 
 const template = fs.readFileSync('template.ejs').toString();
 
-router.use((req, res, next) => {
-  if (/\.template\.html$/.test(req.url)) { res.status(404).end(); }
-  next();
-});
+const staticServer = express.static('public');
 
 router.use((req, res, next) => {
   const filename = path.join(__dirname, 'public', req.path, 'index.template.html');
-  console.log(filename);
-
+  
   if (!fs.existsSync(filename)) {
     return next();
   }
-
+  
   const file = fs.readFileSync(filename, 'utf-8');
+  
+  res.send(renderTemplate(template, file, req.path));
+});
 
-  res.send(renderTemplate(template, file));
+// Don't serve any *.template.html files
+router.use((req, res, next) => {
+  if (/\.template\.html$/.test(req.url)) { return next(); }
+  staticServer(req, res, next);
 });
 
 module.exports = router;
 
-function renderTemplate(template, file) {
+function renderTemplate(template, file, reqPath) {
   const [, head, body] = file.match(/(?:<head>([\s\S]*)<\/head>[\s\S]*)?<body>([\s\S]*)<\/body>/);
 
-  return ejs.render(template, { head, body });
+  return ejs.render(template, { head, body, path: reqPath });
 }
